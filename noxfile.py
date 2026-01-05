5e8c7857ba5bd8165ee253e66514262ed90a10bc
Constantin Müller <info@mueller-constantin.de>
2026-01-05 18:11:05 +0100

    build: combine nox with sphinx

--- noxfile.py
+++ noxfile.py
@@ -34,3 +34,18 @@ def build(session):
     Build package.
     """
     session.run("poetry", "build", external=True)
+
+@nox.session
+def docs(session):
+    """
+    Build documentation with Sphinx.
+    """
+    session.run("poetry", "install", "-E", "pandas", external=True)
+    session.install("-r", "docs/requirements.txt", external=True)
+
+    session.run(
+        "sphinx-build",
+        "-b", "html",
+        "docs/source",
+        "docs/_build/html",
+    )
